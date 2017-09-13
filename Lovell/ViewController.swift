272ff2f4be6f389aa0532c588ffb8f22fe86ed49
TJ Barber <me@tjbarber.org>
2017-09-13 12:48:30 -0600

    Initial Commit

---
+++ Lovell/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Lovell
+//
+//  Created by TJ Barber on 9/13/17.
+//  Copyright © 2017 Novel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
